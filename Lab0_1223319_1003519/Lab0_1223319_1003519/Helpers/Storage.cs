219ab32df96795ab2b7cc66db2a3b1e296a26f5b
Francisco Morales <morales.francisco0206@gmail.com>
2020-02-03 18:43:19 -0600

    Creación del proyecto, del modelo, controlador y vistas

---
+++ Lab0_1223319_1003519/Lab0_1223319_1003519/Helpers/Storage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Lab0_1223319_1003519.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab0_1223319_1003519.Helpers
+{
+    public class Storage
+    {
+        private static Storage _instance;
+
+        public static Storage Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Storage();
+                return _instance;
+            }
+        }
+
+        public List<Cliente> clienteList = new List<Cliente>();
+    }
+}
---
+++ Lab0_1223319_1003519/Lab0_1223319_1003519/Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Lab0_1223319_1003519.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab0_1223319_1003519.Models
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Telefono { get; set; }
+        public string Descripcion { get; set; }
+
+        public bool Save()
+        {
+            try
+            {
+                Storage.Instance.clienteList.Add(this);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
